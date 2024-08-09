f8bf53da7024a35fc3f8b8b6dd0fc70ed5984b4b
Timofey Goncharov <timofigoncharov6112@mail.ru>
2024-08-09 21:24:47 +0300

    code review

--- cypress/e2e/constructor.cy.ts
+++ cypress/e2e/constructor.cy.ts
@@ -1,6 +1,8 @@
 import * as orderFixture from '../fixtures/order.json';
 
 describe('E2E test construcror burger', () => {
+  const dataIngredientSelector = '[data-ingredient="bun"]';
+  const dataIngredientSelectorFirstType = `${dataIngredientSelector}:first-of-type`;
   beforeEach(() => {
     cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients' });
 
@@ -8,7 +10,7 @@ describe('E2E test construcror burger', () => {
   });
 
   it('Array ingredients for select', () => {
-    cy.get('[data-ingredient="bun"]').should('have.length.at.least', 1);
+    cy.get(dataIngredientSelector).should('have.length.at.least', 1);
     cy.get('[data-ingredient="main"],[data-ingredient="sauce"]').should(
       'have.length.at.least',
       1
@@ -18,12 +20,12 @@ describe('E2E test construcror burger', () => {
   describe('Check work modal description ingredients', () => {
     describe('Check open modal', () => {
       it('open card modal', () => {
-        cy.get('[data-ingredient="bun"]:first-of-type').click();
+        cy.get(dataIngredientSelectorFirstType).click();
         cy.get('#modals').children().should('have.length', 2);
       });
 
       it('modal in ingredients open -> reload page', () => {
-        cy.get('[data-ingredient="bun"]:first-of-type').click();
+        cy.get(dataIngredientSelectorFirstType).click();
         cy.reload(true);
         cy.get('#modals').children().should('have.length', 2);
       });
@@ -31,21 +33,21 @@ describe('E2E test construcror burger', () => {
 
     describe('Check close modal', () => {
       it('Click button "x"', () => {
-        cy.get('[data-ingredient="bun"]:first-of-type').click();
+        cy.get(dataIngredientSelectorFirstType).click();
         cy.get('#modals button:first-of-type').click();
         cy.wait(500);
         cy.get('#modals').children().should('have.length', 0);
       });
 
       it('Click overlay', () => {
-        cy.get('[data-ingredient="bun"]:first-of-type').click();
+        cy.get(dataIngredientSelectorFirstType).click();
         cy.get('#modals>div:nth-of-type(2)').click({ force: true });
         cy.wait(500);
         cy.get('#modals').children().should('have.length', 0);
       });
 
       it('Click Escape', () => {
-        cy.get('[data-ingredient="bun"]:first-of-type').click();
+        cy.get(dataIngredientSelectorFirstType).click();
         cy.get('body').type('{esc}');
         cy.wait(500);
         cy.get('#modals').children().should('have.length', 0);
@@ -67,7 +69,7 @@ describe('E2E test construcror burger', () => {
 
     it('Registration after auth', () => {
       cy.get('[data-order-button]').should('be.disabled');
-      cy.get('[data-ingredient="bun"]:first-of-type button').click();
+      cy.get(`${dataIngredientSelector}:first-of-type button`).click();
       cy.get('[data-order-button]').should('be.disabled');
       cy.get('[data-ingredient="main"]:first-of-type button').click();
       cy.get('[data-order-button]').should('be.enabled');
